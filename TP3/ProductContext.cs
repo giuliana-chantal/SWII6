d7962c424c2f7735bf678fc51a52727be56795c1
Giuliana Ferreira Chantal <103539919+giuliana-chantal@users.noreply.github.com>
2023-10-18 21:14:38 -0300

    Add files via upload

---
+++ TP3/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TP3.Models;
+
+namespace TP3
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>().HasKey(m => m.Id);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
